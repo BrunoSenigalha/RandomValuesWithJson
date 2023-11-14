592b84f55d06502b777effb91ee79b77e32d4d48
Bruno Senigalha <brsenigalha@gmail.com>
2023-11-14 17:50:55 -0300

    New refactoring

--- Entities/Mixed.cs
+++ Entities/Mixed.cs
@@ -11,32 +11,29 @@ namespace ChallengeJson.Entities
     {
         public static string[] GeneratingMixedValues(int number)
         {
-            string[] randomLetters = new string[number];
+            Random random = new Random();
 
             string[] letters = { "A","B","C","D","E","F","G","H","I",
                                 "J","K","L","M","N","O","P","Q","R","S",
                                 "T","U","V","X","Z","W","Y"};
 
-            string[] numbers = new string[number];
-            string[] numbersAndLetters = new string[number * 2];
-
-            var random = new Random();
-
-            for (int i = 0; i < number; i++)
-            {
-                numbers[i] = Convert.ToString(random.Next(101));
-                var index = random.Next(letters.Length);
-                randomLetters[i] = letters[index];
-            }
-
-            randomLetters.CopyTo(numbersAndLetters, 0);
-            numbers.CopyTo(numbersAndLetters, randomLetters.Length);
-
             string[] RandomMixed = new string[number];
+
             for (int i = 0; i < number; i++)
             {
-                var index = random.Next(numbersAndLetters.Length);
-                RandomMixed[i] = numbersAndLetters[index];
+                var index = random.Next(letters.Length);
+
+                RandomMixed[i] = random.Next(2) == 0? Convert
+                    .ToString(random.Next(101)):letters[index];
+                //if (random.Next(2) == 0)
+                //{
+                //    RandomMixed[i] = Convert.ToString(random.Next(101));
+                //}
+                //else
+                //{
+                //    var index = random.Next(letters.Length);
+                //    RandomMixed[i] = letters[index];
+                //}
             }
 
             return RandomMixed;
